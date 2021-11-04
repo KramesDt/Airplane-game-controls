6fbc20167557052145265f63f69ca2722eaaa552
Omokorede Adeyeye <lordkratos365@gmail.com>
2021-11-04 17:21:21 +0100

    comment

--- SpinPropellerX.cs
+++ SpinPropellerX.cs
@@ -11,7 +11,7 @@ public class SpinPropellerX : MonoBehaviour
         
     }
 
-    // Update is called once per frame...
+    // Update is called once per frame....
     void Update()
     {
         transform.Rotate(0, 0, speed * Time.fixedDeltaTime, Space.Self);
